1d5fcf9fc911347fc7a8f97d99fa9e584ce320d1
Larry Stein <lars915@live.com>
2020-07-05 09:51:08 -0600

    Playing with sizer

--- wxweather.py
+++ wxweather.py
@@ -4,6 +4,18 @@ class WxWeather(wx.Frame):
     def __init__(self, parent, title):
         super().__init__(parent=parent, title=title)
         self.panel = wx.Panel(self)
+
+        sizer = wx.FlexGridSizer(2, 2, 10, 10)
+        self.panel.SetSizer(sizer)
+        button = wx.Button(self.panel, -1)
+        button2 = wx.Button(self.panel, -1)
+        button3 = wx.Button(self.panel, -1)
+        button4 = wx.Button(self.panel, -1)
+
+        sizer.Add(button)
+        sizer.Add(button2)
+        sizer.Add(button3)
+        sizer.Add(button4)
         self.Show()
 
 
